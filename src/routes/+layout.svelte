34f29755ae12f75f2a5586cb355d49e1d7088646
Luisa Eustaquio <marialuisaeustaquio24@gmail.com>
2026-03-09 05:45:10 +0800

    added buy me a coffee

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,21 @@
 	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<script
+		data-name="BMC-Widget"
+		data-cfasync="false"
+		src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+		data-id="luisaeustaquio"
+		data-description="Support me on Buy me a coffee!"
+		data-message=""
+		data-color="#FFDD00"
+		data-position="Right"
+		data-x_margin="18"
+		data-y_margin="18"
+	></script>
+</svelte:head>
 <div class="relative">
 	<Header />
 	<main>
